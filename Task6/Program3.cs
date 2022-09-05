470c7a7d7d6faf361f5258c36e2a7467fa4f15dc
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 18:00:33 +0700

    Comments were added

--- Task6/Program3.cs
+++ Task6/Program3.cs
@@ -1,10 +1,17 @@
-﻿Console.WriteLine("Введите число: ");
+﻿//Инициализируем ввод числа
+Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
+//При помощи % проверяем
+//делится ли введённое число на 2 без остатка 
+//и вводим новую переменную result
 int result = number % 2;
+
+//Если result больше нуля, то число нечётное. 
+//Если result равно нулю, то число чётное.
 if(result > 0)
 {
-Console.WriteLine($"Число {number} является нечётным");
+    Console.WriteLine($"Число {number} является нечётным");
 }
 
 else
