242b65ab2bf85a3a8569bd4e40a86a9f981109c2
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 19:42:41 +0700

    Description for the program has been added

--- Task8/Program4.cs
+++ Task8/Program4.cs
@@ -1,4 +1,25 @@
-﻿Console.WriteLine("Введите число: ");
+﻿//Производим инициализацию числа
+Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
+//Даём понять пользователю, что программа работает с числами больше 2
+if (number < 2)
+{
+    Console.Write("Программа работает с числами больше ");
+}
+
+//Определяем первоначальное число для вывода. 
+//Оно всегда равно 2. Выводим его.
+int startNumber = 2;
+Console.WriteLine(startNumber);
+
+//Запускаем цикл. 
+//До тех пор пока стартовое число меньше, чем первоначальное -1,
+//прибавляем к нему двойку. 
+
+while (startNumber < number - 1)
+{
+    startNumber = startNumber + 2;
+    Console.WriteLine(startNumber);
+}
 
