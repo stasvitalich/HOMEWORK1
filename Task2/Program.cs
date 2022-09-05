f3f5fedb76a95fafc7b90c34450526ccee3dbdb5
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 15:47:01 +0700

    New conditions has been added. The programm is working correctly.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,4 +4,17 @@ int number1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 
+//Вводим условие, сравнивая между собой первое и второе число. 
+//Если первое число больше, то выводим его. 
+if (number1 > number2)
+{
+    Console.WriteLine($"Из двух введённых чисел большим является число: {number1}");
+}
+
+//Если второе число больше, то выводим его. 
+else
+{
+    Console.WriteLine($"Из двух введённых чисел большим является число: {number2}");
+}
+
 
