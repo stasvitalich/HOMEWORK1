cecef8b6f143e10f5ac58b1676a88cf4c8e85add
Stanislav <stasvitalich1994@gmail.com>
2022-09-05 17:54:39 +0700

    The main part of the program has been added

---
+++ Task6/Program3.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = number % 2;
+if(result > 0)
+{
+Console.WriteLine($"Число {number} является нечётным");
+}
+
+else
+{
+    Console.WriteLine($"Число {number} является чётным");
+}
+
+
+
